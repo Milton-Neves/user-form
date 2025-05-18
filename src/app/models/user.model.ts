fc2f13dcba5e69fac7d4677d9c49748ebd33e4fc
Milton-Neves <milzonzneves@gmail.com>
2025-05-18 18:30:19 -0300

    user-form component created

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export interface User {
+    nome: string;
+    email: string;
+    telefone: string;
+    cpf: string;
+    cep: string;
+    rua: string;
+    bairro: string;
+    cidade: string;
+    estado: string;
+  }
+  
---
+++ src/app/pages/user-form/user-form.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-user-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule],
+  templateUrl: './user-form.component.html',
+  styleUrl: './user-form.component.scss'
+})
+export class UserFormComponent implements OnInit {
+  userForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      cpf: ['', Validators.required],
+      cep: ['', Validators.required],
+      rua: [''],
+      bairro: [''],
+      cidade: [''],
+      estado: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const novoUsuario: User = this.userForm.value;
+      console.log('Usuário cadastrado:', novoUsuario);
+      // Aqui vamos salvar no service depois
+      this.userForm.reset();
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+}
