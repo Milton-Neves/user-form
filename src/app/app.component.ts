256fd3c339584464301d8b65b7e8ea1f648f79b7
Milton-Neves <milzonzneves@gmail.com>
2025-05-19 00:01:03 -0300

    implement rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 import { UserFormComponent } from './pages/user-form/user-form.component';
-import { HttpClient } from '@angular/common/http';
 import { UserListComponent } from './pages/user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserFormComponent, UserListComponent],
+  imports: [CommonModule, RouterOutlet, UserFormComponent, UserListComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -7,7 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-list',
